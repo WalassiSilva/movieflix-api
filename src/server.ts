ac8d27dea937adf3c12590b54d7d0d3e00992e34
Walassi Silva <walassi.b@gmail.com>
2023-12-09 09:09:09 -0300

    add update method with error handling

--- src/server.ts
+++ src/server.ts
@@ -35,8 +35,8 @@ app.post("/movies", async (req, res) => {
             where: { title: { equals: title, mode: "insensitive" } },
         });
 
-        if(duplicate) {
-            return res.status(409).send({message: "Registro ja existente com esse título!"});
+        if (duplicate) {
+            return res.status(409).send({ message: "Registro ja existente com esse título!" });
         }
 
         await prisma.movie.create({
@@ -54,6 +54,66 @@ app.post("/movies", async (req, res) => {
 
     res.status(201).send();
 });
+
+// app.put("/movies/:id", async (req, res) => {
+//     // Pegar o id e mudar pra number
+//     const id = Number(req.params.id);
+
+//     // Pegar a info e atualizar
+//     const data = { ...req.body};
+
+//     // tratar erro para data inválida
+//     data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+//     await prisma.movie.update({
+//         where: { id },
+//         data: 
+//         {
+//             title: data.title,
+//             genre_id: data.genre_id,
+//             language_id: data.language_id,
+//             oscar_count: data.oscar_count,
+//             release_date: new Date(data.release_date),
+//         }
+//     });
+
+//     // Retornar o status correto informando a atualização
+//     res.status(200).send();
+
+// });
+app.put("/movies/:id", async (req, res) => {
+    // Pegar o id como number
+    const id = Number(req.params.id);
+
+    // cobrir todo processo com try/catch
+    try {
+        // Tratar erro pra id inválido
+        const movie = await prisma.movie.findUnique({
+            where: { id }
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "id do filme não encontrado!" });
+        }
+        // Pegar as infos do body
+        const data = { ...req.body };
+
+        // Tratar erro para data inválida
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+        await prisma.movie.update({
+            where: { id },
+            data: data
+        });
+    }
+    catch (error) {
+        res.status(500).send({ message: "Erro ao atualizar registro" });
+    }
+
+    // Rotornar o status correto informado a atualização
+    res.status(200).send();
+});
+
 
 app.listen(port, () => {
     console.log(`Running in http://localhost:${port}`);
